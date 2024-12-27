b238ee53466210656c725115ee8dedda696bf17c
Anustar452 <anustar452@gmail.com>
2024-12-27 10:55:26 +0300

    tailwind css with post css and autoprifixer added, and if there are no records we added no recordes found text with add record option

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    @vite('resources/css/app.css')
+    <title>Lravel Crud</title>
 </head>
 <body>
     <h1>Create a Product</h1>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -8,47 +8,53 @@
 </head>
 <body>
     <h1>Products</h1>
-    <div>
+   
         @if(session()->has('success'))
             <div>
                 {{session('success')}}
             </div>
         @endif
-    </div>
-    <div>
         <table border="1">
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Qty</th>
-                <th>Price</th>
-                <th>Description</th>
-                <th>Edit</th>
-                <th>Delete</th>
-            </tr>
-            
-                @foreach($products as $product)
                 <tr>
-                    <td>{{$product->id}}</td>
-                    <td>{{$product->name}}</td>
-                    <td>{{$product->qty}}</td>
-                    <td>{{$product->price}}</td>
-                    <td>{{$product->description}}</td>
-                    <td>
-                        <a href="{{route('product.edit', ['product' => $product])}}">Edit</a>
-                    </td>
-                    <td>
-                        
-                        <form action="{{route('product.delete', ['product' => $product])}}">
-                            @csrf
-                            @method('DELETE')
-                            <input type="submit" value="Delete">
-                        </form>
-                    </td>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Qty</th>
+                    <th>Price</th>
+                    <th>Description</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
                 </tr>
-                @endforeach
+                    @forelse($products as $product){
+                    <tr>
+                        <td>{{$product->id}}</td>
+                        <td>{{$product->name}}</td>
+                        <td>{{$product->qty}}</td>
+                        <td>{{$product->price}}</td>
+                        <td>{{$product->description}}</td>
+                        <td>
+                            <a href="{{route('product.edit', ['product' => $product])}}">Edit</a>
+                        </td>
+                        <td>
+                            
+                            <form action="{{route('product.delete', ['product' => $product])}}">
+                                @csrf
+                                @method('DELETE')
+                                <input type="submit" value="Delete">
+                            </form>
+                        </td>
+                    </tr>
+                }
+                @empty{
+                    <tr>
+                        <td colspan="5">No Records Found</td>
+                        <td colspan="2">
+                            <a href="{{route('product.create')}}">Add New Record</a>
+                        </td>
+                    </tr>
+                }
+                 @endforelse
             
         </table>
-    </div>
+    
 </body>
 </html>

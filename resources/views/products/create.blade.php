6884c32d502dd4100af4a0bbbcde30058c8f6a98
Anustar452 <anustar452@gmail.com>
2024-12-27 19:03:25 +0300

    integrating tailwind css and styling forms in create.blade.php

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -7,41 +7,44 @@
     @vite('resources/css/app.css')
     <title>Lravel Crud</title>
 </head>
-<body>
-    <h1>Create a Product</h1>
+<body class="bg-white dark:bg-slate-800 px-6 py-8">
+    <h1 class="mb-6 text-3xl font-bold dark:text-white mt-5 text-center">Create a Product</h1>
     <div>
-        @if($errors->any())
-        <ul>
-            @foreach($errors->all() as $error)
-                <li>{{$error}}</li>
-            @endforeach
-        </ul>
-        @endif
+        
+        </div>
+        <div class="mx-auto dark:bg-slate-700 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl w-2/5">
+            @if($errors->any())
+                <ul class="mb-4">
+                    @foreach($errors->all() as $error)
+                        <li class="text-slate-200 dark:to-slate-200">{{$error}}</li>
+                    @endforeach
+                </ul>
+            @endif
+            <form  action="{{route('product.store')}}" method="post" >
+            @csrf
+            @method('post')
+            <div class="mb-4">
+                <label for="" class="text-slate-500 dark:text-slate-400 mt-2 text-sm">Name</label>
+                <input type="text" name="name" class="bg-slate-500 dark:bg-slate-900 mt-1 block w-full rounded-md shadow-sm p-2 focus:ring focus:ring-blue-500 outline-none text-slate-400" placeholder="Name">
+            </div>
+            <div class="mb-4">
+                <label for="" class="text-slate-500 dark:text-slate-400 mt-2 text-sm">Qty</label>
+                <input type="text" name="qty" class="bg-slate-500 dark:bg-slate-900 mt-1 block w-full rounded-md shadow-sm p-2 focus:ring focus:ring-blue-500 outline-none text-slate-400" placeholder="Qty">
+            </div>
+            <div class="mb-4">
+                <label for="" class="text-slate-500 dark:text-slate-400 mt-2 text-sm">Price</label>
+                <input type="text" name="price" class="bg-slate-500 dark:bg-slate-900 mt-1 block w-full rounded-md shadow-sm p-2 focus:ring focus:ring-blue-500 outline-none text-slate-400" placeholder="Price">
+            </div>
+            
+            <div class="mb-4">
+                <label for="" class="text-slate-500 dark:text-slate-400 mt-2 text-sm">Description</label>
+                <input type="text" name="description" class="bg-slate-500 dark:bg-slate-900 mt-1 block w-full rounded-md shadow-sm p-2 focus:ring focus:ring-blue-500 outline-none text-slate-400" placeholder="description">
+            </div>
+            
+            <div class="">
+                <button type="submit" class="bg-slate-200 text-slate-900 dark:bg-slate-800 dark:text-slate-200 font-bold py-2 px-4 w-full text-center rounded hover:bg-slate-900 transition duration-200">Save New Product</button>
+            </div>
+        </form>
     </div>
-    <form class="" action="{{route('product.store')}}" method="post">
-        @csrf
-        @method('post')
-        <div>
-            <label for="">Name</label>
-            <input type="text" name="name" placeholder="Name">
-        </div>
-        <div>
-            <label for="">Qty</label>
-            <input type="text" name="qty" placeholder="Qty">
-        </div>
-        <div>
-            <label for="">Price</label>
-            <input type="text" name="price" placeholder="Price">
-        </div>
-        
-        <div>
-            <label for="">Description</label>
-            <input type="text" name="description" placeholder="description">
-        </div>
-        
-        <div>
-            <button type="submit">Save New Product</button>
-        </div>
-    </form>
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [".resources/"],
+    content: [
+        "./resources/views/products/create.blade.php",
+        "./resources/views/products/edit.blade.php",
+        "./resources/views/products/index.blade.php",
+    ],
     theme: {
         extend: {},
     },
